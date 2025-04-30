c77020ddeea84775f31a5155f222016f10c0f330
Anahat <61635745+AnahatM@users.noreply.github.com>
2025-04-30 10:38:25 -0400

    unused imports

--- src/components/content/DecoratedList.tsx
+++ src/components/content/DecoratedList.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./DecoratedList.css";
 
 interface ListItem {
--- src/components/content/SectionImage.tsx
+++ src/components/content/SectionImage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./SectionImage.css";
 
 interface SectionImageProps {
