239ac2d5c4933a7ebaa40e84b84931e2ae2292d7
silicone-fig <me@siliconecb.cc>
2025-05-01 18:49:50 -0400

    refactor(config): got rid of my unnecessary optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -6,44 +6,4 @@ export default defineConfig({
   plugins: [react()],
 
   base: process.env.GITHUB_ACTIONS ? "/Team-1277-Website/" : "/",
-
-  build: {
-    chunkSizeWarningLimit: 1000,
-    assetsInlineLimit: 4096,
-
-    outDir: 'dist',
-    emptyOutDir: true,
-
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: false,
-        drop_debugger: true
-      }
-    },
-
-    sourcemap: false,
-
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
-  },
-
-  server: {
-    port: 5173,
-    strictPort: false,
-    open: false,
-  },
-
-  preview: {
-    port: 4173,
-    strictPort: false,
-    open: false,
-  },
 });
