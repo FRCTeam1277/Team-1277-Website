2d056e18a323e9bde30c3ed0aa17b90e5511d257
AnahatM <anahat.mudgal@gmail.com>
2025-05-04 16:38:04 -0400

    Add target and rel attributes to GoFundMe link for security and usability

--- src/components/other/GoFundMe.tsx
+++ src/components/other/GoFundMe.tsx
@@ -4,7 +4,9 @@ export default function GoFundMe() {
   return (
     <div className="neuromorphic-inset gofundme">
       <img src="/graphics/GoFundMe_Logo.svg" alt="GoFundMe" width={250} />
-      <a href="https://gofund.me/b2d4baec">Donate</a>
+      <a href="https://gofund.me/b2d4baec" target="_blank" rel="noopener noreferrer">
+        Donate
+      </a>
     </div>
   );
 }
