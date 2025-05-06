3025466ad6dc8f0387d78afbd8d4f83f2de36c2b
AnahatM <anahat.mudgal@gmail.com>
2025-05-05 21:23:56 -0400

    Update CSS for side-by-side items and fix resource links in ProgrammingPage

--- src/pages/members/ProgrammingPage.tsx
+++ src/pages/members/ProgrammingPage.tsx
@@ -16,9 +16,14 @@ export default function ProgrammingPage() {
 
             <TextParagraph text="The programming team is responsible for writing the code that runs on the robot. This includes everything from basic movement to advanced autonomous routines. The programming team also works closely with the electrical team to ensure that all sensors and motors are properly integrated into the robot." />
 
-            <TextParagraph text="[Resources for learning Java](/java-resources)" centered bold removeMargin />
+            <TextParagraph
+              text="[Resources for learning Java](/programming/java-resources)"
+              centered
+              bold
+              removeMargin
+            />
             <LinkImage
-              url="/java-resources"
+              url="/programming/java-resources"
               imagePath="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               altText="Java Resources"
               widthPercent={30}
@@ -26,9 +31,9 @@ export default function ProgrammingPage() {
             />
             <SizedBox height={20} />
 
-            <TextParagraph text="[Software Tools](/software-tools)" centered bold removeMargin />
+            <TextParagraph text="[Software Tools](/programming/software-tools)" centered bold removeMargin />
             <LinkImage
-              url="/software-tools"
+              url="/programming/software-tools"
               imagePath="https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?q=80&w=1500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               altText="Software Tools"
               widthPercent={30}
@@ -36,9 +41,9 @@ export default function ProgrammingPage() {
             />
             <SizedBox height={20} />
 
-            <TextParagraph text="[Programming the Robot](/robot-coding-guide)" centered bold removeMargin />
+            <TextParagraph text="[Programming the Robot](/programming/robot-coding-guide)" centered bold removeMargin />
             <LinkImage
-              url="/robot-coding-guide"
+              url="/programming/robot-coding-guide"
               imagePath="https://upload.wikimedia.org/wikipedia/en/7/70/Terminator1984movieposter.jpg"
               altText="Robot Coding Guide"
               widthPercent={100}
