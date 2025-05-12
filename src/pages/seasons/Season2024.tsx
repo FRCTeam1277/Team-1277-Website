12efb6cdeac85d7f757b9cc0d582a5ad73f3b141
Anahat <61635745+AnahatM@users.noreply.github.com>
2025-05-12 09:06:19 -0400

    2024 page content

--- src/pages/seasons/Season2024.tsx
+++ src/pages/seasons/Season2024.tsx
@@ -3,7 +3,6 @@ import ContentSection from "../../components/content/ContentSection";
 import SizedBox from "../../components/utils/SizedBox";
 import TextParagraph from "../../components/content/TextParagraph";
 import SectionImage from "../../components/content/SectionImage";
-import LinkButton from "../../components/content/LinkButton";
 import SponsorGrid from "../../components/content/SponsorGrid";
 import DecoratedList from "../../components/content/DecoratedList";
 
@@ -13,26 +12,36 @@ export default function Season2024Page() {
       <BannerImage imagePath={"/pictures/2024/2024_Team-Picture-Edited.jpg"} />
 
       <ContentSection
-        title={"2025 Competition Season"}
-        subtitle={"Making Waves on the Reefscape"}
+        title={"2024 Competition Season"}
+        subtitle={"Crescendo presented by Haas"}
         content={
           <>
             <TextParagraph
-              text="Beneath the ocean's surface lies our planet's most complex ecosystems, full of life and potential for
-              exploration and learning, where each inhabitant has a role to play in building a thriving environment.
-              Teams use their STEM and collaboration skills to explore life beneath the surface of the ocean."
+              text="In CRESCENDO presented by Haas, two competing alliances are invited to score notes, amplify their speaker,
+              harmonize onstage, and take the spotlight before time runs out. Alliances earn additional rewards for meeting
+              specific scoring thresholds and for cooperating with their opponents."
             />
             <TextParagraph
-              text="Along the way, we uncover the potential in each of us to strengthen our community and innovate for a
-              better world with healthy oceans."
+              text="The game is themed around music and involves two alliances consisting of three teams each competing 
+              to perform various tasks, including shooting rings known as Notes into goals, and hanging on chains at the end of the match."
               emphasis={true}
             />
             <TextParagraph
-              text="Two alliances of three robots each work to score PVC pipes (called Coral) and playground balls (called
-              Algae) into goals. At the end of the match, robots move to a truss structure called the Barge and climb
-              metal Cages to earn additional points."
+              text="During the first 15 seconds of the match, robots are autonomous. Without guidance from their drivers, robots
+              leave their starting zone, score notes in their speaker or amp, and collect and score additional notes.
+              During the remaining 2 minutes and 15 seconds, drivers control their robots. Robots collect notes from human
+              players at their source and score them in their amp and speaker."
             />
-            <SectionImage imagePath="/pictures/2025/2025_Dive_Banner.jpg" widthPercent={50} disableClick={true} />
+            <TextParagraph
+              text="As time runs out, robots race to get onstage and deliver notes to their traps. Harmonizing robots, i.e. robots
+              sharing a chain, earn an added bonus. Robots earn even more points if a human player spotlights robots on a
+              chain by scoring a note on the chain's microphone."
+            />
+            <SectionImage
+              imagePath="https://upload.wikimedia.org/wikipedia/en/thumb/8/88/Crescendo_FRC_Logo.svg/1200px-Crescendo_FRC_Logo.svg.png"
+              widthPercent={50}
+              disableClick={true}
+            />
             <SizedBox height={40} />
           </>
         }
@@ -40,37 +49,31 @@ export default function Season2024Page() {
 
       <ContentSection
         title={"Events"}
-        subtitle={"Competitions and Events that we attended in the 2025 season"}
+        subtitle={"Competitions and Events that we attended in the 2024 season"}
         centerText={true}
         content={
           <>
-            <TextParagraph text="Come see us at Battle Cry at WPI, on June 6, 7 and 8!" primary={true} />
-            <TextParagraph
-              text="[Salem HS](https://frc-events.firstinspires.org/2025/NHSAL): 
-              Friday, February 28 to Sunday, March 2, 2025 (Week 1)"
-            />
+            <TextParagraph text="NE District Granite State Event" primary={true} removeMargin />
+            <TextParagraph text="February 29 – March 2, 2024" bold={true} removeMargin />
+            <TextParagraph text="Salem High School, 44 Geremonty Drive, Salem, NH 03079" />
 
-            <LinkButton
-              url={"https://firstnh.wixsite.com/firstnh/gsd-team-information"}
-              textContent={"Event Info"}
-              icon="/icons/calendar.png"
-              invertIcon={true}
-            />
-            <SizedBox height={30} />
+            <SizedBox height={20} />
+
+            <TextParagraph text="NE District North Shore Event" primary={true} removeMargin />
+            <TextParagraph text="March 15 – March 17, 2024" bold={true} removeMargin />
+            <TextParagraph text="Reading Memorial High School, 62 Oakland Road, Reading, MA 01867" />
+
+            <SizedBox height={20} />
 
             <TextParagraph
-              text="NE FIRST will stream match play to [nefirst.org/livestream](http://nefirst.org/livestream). 
-              Northern events will stream to BLUE links. Southern events will stream to RED links. 
-              Third events for a week will stream to PURPLE links."
+              text="NE FIRST streamed match play to [nefirst.org/livestream](http://nefirst.org/livestream). 
+              Northern events streamed to BLUE links. Southern events streamed to RED links. 
+              Third events for a week streamed to PURPLE links."
             />
-            <TextParagraph text="[WPI](https://frc-events.firstinspires.org/2025/MAWOR): Friday, March 21 to Sunday, March 23, 2025 (Week 4)" />
-            <TextParagraph
-              text="Full list of New England Competitions can be found 
-              [here](https://frc-events.firstinspires.org/2025/Events/EventList?filter=NE)."
-            />
+
             <SectionImage
-              imagePath="/pictures/2025/2025_WPI-Team-Picture.jpg"
-              caption="Proud to have finished qualifiers in 8th place becoming Alliance Captains for Alliance 5. WPI Competition, March 23, 2025."
+              imagePath="/pictures/2024/2024_Industrial-Design-Award.jpg"
+              caption="Our team proudly receiving the Industrial Design Award at the 2024 North Shore Event."
             />
             <SizedBox height={40} />
           </>
@@ -78,7 +81,7 @@ export default function Season2024Page() {
       />
 
       <ContentSection
-        title={"2025 Team Support"}
+        title={"2024 Team Support"}
         subtitle={"We are grateful for the support making this season possible!"}
         centerText={true}
         content={
@@ -90,25 +93,20 @@ export default function Season2024Page() {
               items={[
                 { imagePath: "/sponsors/BAE_Logo.png", link: "https://www.baesystems.com/en-us/home" },
                 { imagePath: "/sponsors/RTX_Raytheon_Technologies_logo.webp", link: "https://www.rtx.com/" },
-                { imagePath: "/sponsors/HPE_Logo.png", link: "https://www.hpe.com/us/en/home.html" },
                 { imagePath: "/sponsors/GDEF_Logo.png", link: "https://www.gdefinc.org/" },
+                { imagePath: "/sponsors/HPE_Logo.png", link: "https://www.hpe.com/us/en/home.html" },
                 {
-                  imagePath: "/sponsors/national-carpet-flooring-logo.png",
-                  link: "https://www.nationalcarpetandflooring.com/",
+                  imagePath:
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/AMD_LOGO.svg/2560px-AMD_LOGO.svg.png",
+                  link: "https://www.amd.com/",
                 },
                 {
-                  imagePath: "https://www.sterilite.com/images/sterilite_logo.png",
-                  link: "https://www.sterilite.com/",
+                  imagePath:
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Microsoft_logo_%282012%29.svg/2560px-Microsoft_logo_%282012%29.svg.png",
+                  link: "https://www.microsoft.com/",
                 },
               ]}
             />
-
-            <TextParagraph text="Additionally, we want to thank our mentors, who have been instrumental in guiding our team with expertise and encouragement throughout the season." />
-
-            <TextParagraph text="Special thanks to" primary={true} removeMargin={true} />
-            <TextParagraph text="The McKinley Family" bold={true} uppercase={true} removeMargin={true} />
-            <TextParagraph text="The Hughson Family" bold={true} uppercase={true} removeMargin={true} />
-            <TextParagraph text="The Goss Family" bold={true} uppercase={true} removeMargin={true} />
 
             <SizedBox height={40} />
           </>
@@ -123,16 +121,16 @@ export default function Season2024Page() {
               items={[
                 { title: "Stone Age: $1-99", description: "Thank you!" },
                 { title: "Bronze Age: $100-499", description: "Company name on website + social media shoutout" },
-                { title: "Iron Age: $500-999", description: "What’s above + name on tee shirt" },
+                { title: "Iron Age: $500-999", description: "What's above + name on tee shirt" },
                 {
                   title: "Industrial Revolution: $1000-1999",
-                  description: "What’s above + name on banner and small name on robot",
+                  description: "What's above + name on banner and small name on robot",
                 },
-                { title: "Machine Age: $2000-3999", description: "What’s above + large name on robot" },
+                { title: "Machine Age: $2000-3999", description: "What's above + large name on robot" },
                 {
                   title: "Digital Revolution: $4000+",
                   description:
-                    "What’s above + preferred location of large name on robot and end of year robot demo at company",
+                    "What's above + preferred location of large name on robot and end of year robot demo at company",
                 },
               ]}
             />
