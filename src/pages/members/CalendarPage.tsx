06179bcd11d079042153a7f77e265f04d931286f
AnahatM <anahat.mudgal@gmail.com>
2025-05-25 20:18:10 -0400

    Embed Google Calendar

--- src/pages/members/CalendarPage.tsx
+++ src/pages/members/CalendarPage.tsx
@@ -12,10 +12,18 @@ export default function CalendarPage() {
           <>
             <TextParagraph text="The Robotomies calendar is a great way to keep track of all the events, competitions, and meetings that are coming up.  We have a lot of events planned for this year, so be sure to check back often!" />
             <TextParagraph
-              text="Calendar coming to website soon. If its still not here, contact Mark Rocheleau."
+              text="This is the official Robotomies events Google Calendar, updated regularly."
               centered
               bold
               primary
+            />
+            <iframe
+              src="https://calendar.google.com/calendar/u/0/embed?src=07ca5e9afc5e07881dccad0d61e78323f3124c0630be7e41d4ff90998049991f@group.calendar.google.com&ctz=America/New_York"
+              style={{ border: 0 }}
+              width="800"
+              height="600"
+              frameBorder="0"
+              scrolling="no"
             />
           </>
         }
