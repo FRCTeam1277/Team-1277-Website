223fd1a012b707b24631650902d3305dc0c23eaf
AnahatM <anahat.mudgal@gmail.com>
2025-08-29 13:13:30 -0400

    Update vite ports

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-const isCustomDomain = process.env.CUSTOM_DOMAIN === 'true';
+const isCustomDomain = process.env.CUSTOM_DOMAIN === "true";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    port: 5178,
+  },
   base: process.env.GITHUB_ACTIONS && !isCustomDomain ? "/Team-1277-Website/" : "/",
 });
-
